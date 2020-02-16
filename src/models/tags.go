1c1b70ef1221f24ae4897c203911db451aa3e8a8
Joaquim Alves <joaquimjunior13@gmail.com>
2020-02-16 13:10:57 -0300

    funçao de get atualizada e funcional, falta inserir dados no banco

--- src/models/tags.go
+++ src/models/tags.go
@@ -3,24 +3,24 @@ package models
 type Tags struct {
 	Data struct {
 		User struct {
-			ID                  string `bson:"id_user" json:"id"`
+			ID                  string `json:"id"`
 			StarredRepositories struct {
 				Edges []struct {
 					Node struct {
-						ID          string `bson: "id_repository" json:"id"`
-						Description string `bson: "description" json:"description"`
+						ID          string `json:"id"`
+						Description string `json:"description"`
 						Languages   struct {
 							Edges []struct {
 								Node struct {
-									Name string `bson:"name_language" json:"name"`
-								} `bson:"node_language" json:"node"`
-							} `bson:"edges_language" json:"edges"`
-						} `bson:"languages" json:"languages"`
-						Name        string `bson:"name_repository" json:"name"`
-						ProjectsURL string `bson:"projectsURL" json:"projectsUrl"`
-					} `bson:"node_repository" json:"node"`
-				} `bson:"edges_repository" json:"edges"`
-			} `bson:"starredRepositories" json:"starredRepositories"`
-		} `bson:"user" json:"user"`
-	} `bson:"data" json:"data"`
+									Name string `json:"name"`
+								} `json:"node"`
+							} `json:"edges"`
+						} `json:"languages"`
+						Name        string `json:"name"`
+						ProjectsURL string `json:"projectsUrl"`
+					} `json:"node"`
+				} `json:"edges"`
+			} `json:"starredRepositories"`
+		} `json:"user"`
+	} `json:"data"`
 }
--- src/router/tags_router.go
+++ src/router/tags_router.go
@@ -3,18 +3,25 @@ package router
 import (
 	"bytes"
 	"encoding/json"
-	"io/ioutil"
+	"fmt"
+	"io"
 	"log"
 	"net/http"
 	"tagsForGit-api/src/config/dao"
 	"tagsForGit-api/src/models"
 
 	"github.com/gorilla/mux"
+	"github.com/pkg/errors"
 )
 
 var daoTags = dao.TagsDAO{}
+
+type graphResponse struct {
+	Data interface{}
+}
+
 var urlGraphQL = "https://api.github.com/graphql"
-var token = "c210bd6cd82948ce7b1830bc84de3c495801a8fa"
+var token = "14556d98644a4a02fe68d9325cc914020f7a4602"
 
 func respondWithError(w http.ResponseWriter, code int, msg string) {
 	respondWithJson(w, code, map[string]string{"error": msg})
@@ -28,6 +35,8 @@ func respondWithJson(w http.ResponseWriter, code int, payload interface{}) {
 }
 
 func GetAllTags(w http.ResponseWriter, r *http.Request) {
+	//Função que recebe a requisição para buscara todas as tags que o usuário possui
+	//Validação se recebeu o userlogin para buscar na api do git
 	userLogin, ok := r.URL.Query()["userLogin"]
 	if !ok || len(userLogin[0]) < 1 {
 		log.Println("Url Param 'userLogin' is missing")
@@ -35,43 +44,65 @@ func GetAllTags(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	// Realiza o processo de converter a string para o formato do graphql
-
-	client := &http.Client{}
+	//Criação da query de login usando estrutura de query
+	//A estrutura pode evoluir para separar a query das variáveis
 	var queryString = models.GetRepositories(userLogin[0])
-
 	requestBodyObj := struct {
 		Query string `json:"query"`
 	}{
 		Query: queryString}
+	//Encoder da estrutura de query para bytes
 	var requestBody bytes.Buffer
 	if err := json.NewEncoder(&requestBody).Encode(requestBodyObj); err != nil {
 		respondWithError(w, http.StatusInternalServerError, err.Error())
 		return
 	}
-
-	//var queryBytes = bytes.NewBufferString(queryString)
+	//Início das configurações da requisição http
+	client := &http.Client{}
 	req, _ := http.NewRequest("POST", urlGraphQL, &requestBody)
 	req.Header.Set("Authorization", "Bearer "+token)
 	log.Println(req.Body)
 	res, err := client.Do(req)
+	//fim da requisição
 	if err != nil {
 		respondWithError(w, http.StatusInternalServerError, err.Error())
+	}
+	defer res.Body.Close()
+	//Pega o resultado que vem no body e copia para o buffer
+	var buf bytes.Buffer
+	if _, err := io.Copy(&buf, res.Body); err != nil {
+		log.Println(errors.Wrap(err, "reading body"))
 		return
 	}
-	log.Println(res.StatusCode)
-	if res.StatusCode == 200 {
-		var tags = models.Tags{}
-		getJson(res, tags)
-		respondWithError(w, 200, "ok")
-		log.Println("Url Param 'userLogin' is here")
-		return
+	gr := &graphResponse{
+		Data: models.Tags{},
 	}
 
-	b, _ := ioutil.ReadAll(res.Body)
-	log.Fatal(string(b))
-	respondWithError(w, 200, "ok")
-	// body, err := ioutil.ReadAll(res.Body)
+	if err := json.NewDecoder(&buf).Decode(&gr); err != nil {
+		if res.StatusCode != http.StatusOK {
+			log.Println(fmt.Errorf("graphql: server returned a non-200 status code: %v", res.StatusCode))
+		}
+		log.Println(errors.Wrap(err, "decoding response"))
+	}
+	// if err != nil {
+	// 	respondWithError(w, http.StatusInternalServerError, err.Error())
+	// 	return
+	// }
+	// log.Println(res.StatusCode)
+	// if res.StatusCode == 200 {
+	// 	log.Println(res.Body)
+	// 	var tags = getJson(res, models.Tags{})
+	// 	// var tags []models.Tags
+	// 	// tags = daoTags.GetAllTags()
+	// 	respondWithJson(w, 200, tags)
+	// 	log.Println("Url Param 'userLogin' is here")
+	// 	return
+	// }
+
+	// b, _ := ioutil.ReadAll(res.Body)
+	// log.Fatal(string(b))
+	respondWithJson(w, 200, gr)
+	// body, err := ioutil.ReadAll(res.Body)gr
 	// if err != nil {
 	// 	respondWithError(w, http.StatusInternalServerError, err.Error())
 	// }
